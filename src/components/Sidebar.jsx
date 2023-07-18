cf45028117c0a439220116b5c27fb25643863cb9
Prabhat-Mani <prabhat@vcbay.co>
2023-07-18 16:05:24 +0530

    Updated chats fixed bugs!

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,22 +1,34 @@
 import React from "react";
-import { Link, useParams } from 'react-router-dom';
+import { Link, useParams } from "react-router-dom";
 
 const Sidebar = () => {
-  const {id} = useParams();
+  const { id } = useParams();
 
   return (
     <div className="bg-blue-500 px-10 flex flex-col gap-6 items-center justify-center w-full h-[50rem] rounded-3xl">
-      <Link className="border-gray-300 border-b-[1px] w-full pb-4" to={`/profile/${id}`}>
+      <Link
+        className="border-gray-300 border-b-[1px] w-full pb-4"
+        to={`/profile/${id}`}
+      >
         <h1 className="text-white font-bold text-xl">Profile</h1>
       </Link>
-      <Link className="border-gray-300 border-b-[1px] w-full pb-4" to={`/posts/${id}`}>
-        <h1 className="text-gray-300 text-xl">Posts</h1>
+      <Link
+        className="border-gray-300 border-b-[1px] w-full pb-4"
+        to={`/posts/${id}`}
+      >
+        <h1 className="text-gray-300 font-bold text-xl">Posts</h1>
       </Link>
-      <Link className="border-gray-300 border-b-[1px] w-full pb-4" to={`/gallery/${id}`}>
-        <h1 className="text-gray-300 text-xl">Gallery</h1>
+      <Link
+        className="border-gray-300 border-b-[1px] w-full pb-4"
+        to={`/gallery/${id}`}
+      >
+        <h1 className="text-gray-300 font-bold text-xl">Gallery</h1>
       </Link>
-      <Link className="border-gray-300 border-b-[1px] w-full pb-4" to={`/todo/${id}`}>
-        <h1 className="text-gray-300 text-xl">Todo</h1>
+      <Link
+        className="border-gray-300 border-b-[1px] w-full pb-4"
+        to={`/todo/${id}`}
+      >
+        <h1 className="text-gray-300 font-bold text-xl">Todo</h1>
       </Link>
     </div>
   );
